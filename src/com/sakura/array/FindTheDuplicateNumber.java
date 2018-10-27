9b0f1d8e3f4bc670187387d0f4c2a95750486aec
sakura-hly <1612584677@qq.com>
2018-10-27 22:11:48 +0800

    array/FindTheDuplicateNumber

---
+++ src/com/sakura/array/FindTheDuplicateNumber.java
@@ -0,0 +1,21 @@
+package com.sakura.array;
+
+public class FindTheDuplicateNumber {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0], fast = nums[nums[0]];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        fast = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new FindTheDuplicateNumber().findDuplicate(new int[]{1, 3, 4, 2, 2}));
+    }
+}
