d317b4f4cf8a5ce079d591e80ecc3f652ec61f2f
sakura-hly <1612584677@qq.com>
2018-10-28 18:58:31 +0800

    array/RotateImage

---
+++ src/com/sakura/array/RotateImage.java
@@ -0,0 +1,25 @@
+package com.sakura.array;
+
+public class RotateImage {
+    public void rotate(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return;
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                if (i != j) {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
+            }
+        }
+        for (int j = 0; j < matrix.length / 2; j++) {
+            for (int i = 0; i < matrix.length; i++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
+        }
+    }
+}
