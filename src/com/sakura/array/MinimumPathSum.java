a0349f887ba0d13370f84ec19add7fff2e8a00c0
sakura-hly <1612584677@qq.com>
2018-10-28 22:06:17 +0800

    array/MinimumPathSum

---
+++ src/com/sakura/array/MinimumPathSum.java
@@ -0,0 +1,35 @@
+package com.sakura.array;
+
+public class MinimumPathSum {
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int[][] dp = new int[grid.length][grid[0].length];
+        dp[0][0] = grid[0][0];
+        for (int j = 1; j < grid[0].length; j++) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+        for (int i = 1; i < grid.length; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int i = 1; i < grid.length; i++) {
+            for (int j = 1; j < grid[0].length; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[grid.length - 1][grid[0].length - 1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(
+                new MinimumPathSum().minPathSum(
+                        new int[][]{
+//                                {1, 3, 1},
+//                                {1, 5, 1},
+//                                {4, 2, 1}
+                                {1, 2},
+                                {1, 1}
+                        }));
+    }
+}
