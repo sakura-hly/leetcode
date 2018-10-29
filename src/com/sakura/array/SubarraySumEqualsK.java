39988dfaff101f70d83693b6f86536311062c905
sakura-hly <1612584677@qq.com>
2018-10-29 23:01:33 +0800

    array/SubarraySumEqualsK

---
+++ src/com/sakura/array/SubarraySumEqualsK.java
@@ -0,0 +1,21 @@
+package com.sakura.array;
+
+public class SubarraySumEqualsK {
+    public int subarraySum(int[] nums, int k) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                if (sum == k) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new SubarraySumEqualsK().subarraySum(new int[]{1, 1, 1}, 2));
+    }
+}
