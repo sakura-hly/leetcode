e4b6412a5debcbc0134f46263a97486db6b803da
sakura-hly <1612584677@qq.com>
2018-10-29 10:01:57 +0800

    Create PalindromicSubstrings.java

---
+++ src/com/sakura/dynamic/PalindromicSubstrings.java
@@ -0,0 +1,42 @@
+package com.sakura.dynamic;
+
+public class PalindromicSubstrings {
+    public int countSubstrings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int len = s.length();
+        int result = 0;
+        for (int i = 0; i < len; i++) {
+            result = getResult(s, result, i, i);//Count even sized
+            result = getResult(s, result, i, i + 1);//Count odd sized
+        }
+        return result;
+    }
+
+    private int getResult(String s, int result, int i, int j) {
+        while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)) {
+            result++;
+            i--;
+            j++;
+        }
+        return result;
+    }
+
+
+    public static boolean IsPalindromic(String s) {
+        if (s.length() <= 1) {
+            return true;
+        }
+        for (int i = 0, j = s.length() - 1; i <= j; i++, j--) {
+            if (!(s.charAt(i) == s.charAt(j))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new PalindromicSubstrings().countSubstrings("aaa"));
+    }
+}
