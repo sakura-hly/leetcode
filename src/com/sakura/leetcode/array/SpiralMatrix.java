09984177a3affdca0ede6ad80c1f15718190ec3b
sakura-hly <1612584677@qq.com>
2019-01-10 21:48:56 +0800

    SpiralMatrix

---
+++ src/com/sakura/leetcode/array/SpiralMatrix.java
@@ -0,0 +1,50 @@
+package com.sakura.leetcode.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralMatrix {
+
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>(matrix.length * matrix[0].length);
+        if (matrix == null || matrix.length == 0) {
+            return result;
+        }
+        int beginRow = 0, endRow = matrix.length - 1, beginCol = 0, endCol = matrix[0].length - 1;
+        while (beginRow <= endRow && beginCol <= endCol) {
+            for (int j = beginCol; j <= endCol; j++) {
+                result.add(matrix[beginRow][j]);
+            }
+            beginRow++;
+            for (int i = beginRow; i <= endRow; i++) {
+                result.add(matrix[i][endCol]);
+            }
+            endCol--;
+            if (beginRow <= endRow) {
+                for (int j = endCol; j >= beginCol; j--) {
+                    result.add(matrix[endRow][j]);
+                }
+                endRow--;
+            }
+            if (beginCol <= endCol) {
+                for (int i = endRow; i >= beginRow; i--) {
+                    result.add(matrix[i][beginCol]);
+                }
+                beginCol++;
+            }
+        }
+
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(
+                new SpiralMatrix().spiralOrder(
+                        new int[][]{
+                                {1, 2, 3},
+                                {4, 5, 6},
+                                {7, 8, 9}
+                        }));
+    }
+}
