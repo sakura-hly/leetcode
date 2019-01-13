952527b4e13a9137ae8e5933d8e6195d496d0fd2
sakura-hly <1612584677@qq.com>
2019-01-13 16:17:44 +0800

    OddEvenLinkedList

---
+++ src/com/sakura/leetcode/linkedlist/OddEvenLinkedList.java
@@ -0,0 +1,30 @@
+package com.sakura.leetcode.linkedlist;
+
+public class OddEvenLinkedList {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+
+        ListNode odd = head, even = head.next, evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+
+            odd = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+
+        return head;
+    }
+}
