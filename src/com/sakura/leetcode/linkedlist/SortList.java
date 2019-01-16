eba72912c1dcfc331822b3aa2c073871c30d9834
sakura-hly <1612584677@qq.com>
2019-01-16 22:34:19 +0800

    SortList

---
+++ src/com/sakura/leetcode/linkedlist/SortList.java
@@ -0,0 +1,53 @@
+package com.sakura.leetcode.linkedlist;
+
+public class SortList {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode sortList(ListNode head) {
+        if (head == null && head.next == null) {
+            return head;
+        }
+        ListNode pre = null, slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        pre.next = null;
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(slow);
+
+        return merge(l1, l2);
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(-1), cur = result;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+
+        if (l1 != null) {
+            cur.next = l1;
+        }
+        if (l2 != null) {
+            cur.next = l2;
+        }
+        return result.next;
+
+    }
+}
