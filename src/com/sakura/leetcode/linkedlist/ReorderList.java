26d07a4dc8bae4a290458d71580ea7048928e77e
sakura-hly <1612584677@qq.com>
2019-01-17 23:02:40 +0800

    ReorderList

---
+++ src/com/sakura/leetcode/linkedlist/ReorderList.java
@@ -0,0 +1,49 @@
+package com.sakura.leetcode.linkedlist;
+
+public class ReorderList {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        ListNode slow = head, fast = head, pre = null;
+        while (fast != null && fast.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        pre.next = null;
+        ListNode l1 = head, l2 = slow;
+
+        ListNode cur = l2;
+        pre = null;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        l2 = pre;
+
+        while (l1 != null) {
+            ListNode n1 = l1.next, n2 = l2.next;
+            l1.next = l2;
+            if (n1 == null) {
+                break;
+            }
+            l2.next = n1;
+            l1 = n1;
+            l2 = n2;
+        }
+    }
+}
