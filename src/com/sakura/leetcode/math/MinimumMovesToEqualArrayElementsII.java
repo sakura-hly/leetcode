7b4ebc3f31215eabfc4b9f562d576b76812d1fcb
sakura-hly <1612584677@qq.com>
2019-01-18 21:45:13 +0800

    MinimumMovesToEqualArrayElementsII

---
+++ src/com/sakura/leetcode/math/MinimumMovesToEqualArrayElementsII.java
@@ -0,0 +1,24 @@
+package com.sakura.leetcode.math;
+
+import java.util.Arrays;
+
+public class MinimumMovesToEqualArrayElementsII {
+
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+
+        int i = 0, j = nums.length - 1;
+        int result = 0;
+
+        while (i < j) {
+            result += nums[j] - nums[i];
+            i++;
+            j--;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new MinimumMovesToEqualArrayElementsII().minMoves2(new int[]{1, 2, 3}));
+    }
+}
