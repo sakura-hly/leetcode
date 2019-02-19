4ae598c57a48dc78a49896db09a82d52b99bd198
sakura-hly <1612584677@qq.com>
2019-02-19 20:58:05 +0800

    QueueReconstructionByHeight

---
+++ src/com/sakura/leetcode/top10like/QueueReconstructionByHeight.java
@@ -0,0 +1,23 @@
+package com.sakura.leetcode.top10like;
+
+import java.util.Arrays;
+
+public class QueueReconstructionByHeight {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (o1, o2) -> o1[0] == o2[0] ? o1[1] - o2[1] : o2[0] - o1[0]);
+        int[][] result = new int[people.length][2];
+        for (int i = 0; i < people.length; i++) {
+            int pos = people[i][1];
+            for (int j = i; j > pos; j--) {
+                result[j] = result[j - 1];
+            }
+            result[pos] = people[i];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        new QueueReconstructionByHeight().reconstructQueue(new int[][]{{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}});
+    }
+}
