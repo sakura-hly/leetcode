f2b9e7a068a789c1dacc2bb4e440b64ff84963ab
sakura-hly <1612584677@qq.com>
2019-02-23 23:16:47 +0800

    MaximalRectangle

---
+++ src/com/sakura/leetcode/top100like/MaximalRectangle.java
@@ -0,0 +1,50 @@
+package com.sakura.leetcode.top100like;
+
+import java.util.Arrays;
+
+public class MaximalRectangle {
+
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix == null || matrix.length == 0) return 0;
+        int m = matrix.length, n = matrix[0].length;
+        int[] left = new int[n], right = new int[n], height = new int[n];
+        Arrays.fill(right, n);
+        int result = 0;
+
+        for (int i = 0; i < m; i++) {
+            int curLeft = 0, curRight = n;
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    height[j]++;
+                    left[j] = Math.max(left[j], curLeft);
+                } else {
+                    height[j] = 0;
+                    left[j] = 0;
+                    curLeft = j + 1;
+                }
+            }
+
+            for (int j = n - 1; j >= 0; j--) {
+                if (matrix[i][j] == '1') {
+                    right[j] = Math.min(right[j], curRight);
+                } else {
+                    right[j] = n;
+                    curRight = j;
+                }
+                result = Math.max(result, height[j] * (right[j] - left[j]));
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(
+                new MaximalRectangle().maximalRectangle(
+                        new char[][]{
+                                {'1', '0', '1', '0', '0'},
+                                {'1', '0', '1', '1', '1'},
+                                {'1', '1', '1', '1', '1'},
+                                {'1', '0', '0', '1', '0'}
+                        }));
+    }
+}
