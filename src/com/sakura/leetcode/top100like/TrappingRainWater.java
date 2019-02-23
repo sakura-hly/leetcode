aa121d884b1498ad8cbceac36dec13ba28b6f5bd
sakura-hly <1612584677@qq.com>
2019-02-23 13:36:23 +0800

    TrappingRainWater

---
+++ src/com/sakura/leetcode/top100like/TrappingRainWater.java
@@ -0,0 +1,33 @@
+package com.sakura.leetcode.top100like;
+
+public class TrappingRainWater {
+
+    public int trap(int[] height) {
+        int left = 0, right = height.length - 1;
+        int result = 0;
+        int leftMax = 0, rightMax = 0;
+        while (left < right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    result += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    result += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new TrappingRainWater().trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+    }
+}
