84a794d6a19aa039de33e37050795ab13929f04f
sakura-hlt <1612584677@qq.com>
2019-12-04 17:42:57 +0800

    MyStack

---
+++ src/com/sakura/leetcode/stack/MyStack.java
@@ -0,0 +1,49 @@
+package com.sakura.leetcode.stack;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * https://leetcode.com/problems/implement-stack-using-queues/
+ */
+public class MyStack {
+
+    Queue<Integer> queue = new LinkedList<>();
+
+    /** Initialize your data structure here. */
+    public MyStack() {
+
+    }
+
+    /** Push element x onto stack. */
+    public void push(int x) {
+        queue.offer(x);
+        for (int i = 0; i < queue.size() - 1; i++) {
+            queue.offer(queue.poll());
+        }
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return queue.poll();
+    }
+
+    /** Get the top element. */
+    public int top() {
+        return queue.peek();
+    }
+
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
