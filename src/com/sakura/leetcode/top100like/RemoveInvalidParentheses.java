54a71b8f2cae38e8038b81d0105544eae95c0fbc
sakura-hly <1612584677@qq.com>
2019-12-27 09:55:29 +0000

    add removeInvalidParentheses bfs version

--- src/com/sakura/leetcode/top100like/RemoveInvalidParentheses.java
+++ src/com/sakura/leetcode/top100like/RemoveInvalidParentheses.java
@@ -2,7 +2,9 @@ package com.sakura.leetcode.top100like;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Set;
 
 public class RemoveInvalidParentheses {
@@ -62,6 +64,49 @@ public class RemoveInvalidParentheses {
             return;
         }
     }
+
+    /*********************************  BFS *******************************************/
+    public List<String> removeInvalidParenthesesBfs(String s) {
+        List<String> list = new LinkedList<>();
+
+        Set<String> set = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(s);
+        set.add(s);
+        while (!queue.isEmpty()) {
+            String str = queue.poll();
+            if (validParentheses(str)) {
+                list.add(str);
+                continue;
+            }
+            if (list.size() > 0) continue; // bfs, so next round can't be the minimum
+
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) != '(' && str.charAt(i) != ')') continue;
+                String next = str.substring(0, i) + str.substring(i + 1);
+                if (!set.contains(next)) {
+                    queue.offer(next);
+                    set.add(next);
+                }
+            }
+        }
+
+        return list;
+    }
+
+    private boolean validParentheses(String str) {
+        int count = 0;
+        for (char c : str.toCharArray()) {
+            if (c == '(') {
+                count++;
+            } else if (c == ')') {
+                count--;
+            }
+            if (count < 0) return false;
+        }
+        return count == 0;
+    }
+
 
     public static void main(String[] args) {
         System.out.println(new RemoveInvalidParentheses().removeInvalidParentheses("()())()"));
