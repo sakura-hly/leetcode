649c5c3f54adf63606fca0d7174b48b14cbbcb5a
sakura-hly <1612584677@qq.com>
2020-01-12 16:10:59 +0800

    PostorderTraversal

---
+++ src/com/sakura/leetcode/tree/PostorderTraversal.java
@@ -0,0 +1,90 @@
+package com.sakura.leetcode.tree;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class PostorderTraversal {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new LinkedList<>();
+        if (root == null) return list;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        TreeNode prev = null;
+        while (!stack.isEmpty()) {
+            TreeNode curr = stack.peek();
+            if ((curr.right != null && curr.right == prev) 
+            || (curr.left != null && curr.left == prev)
+            || (curr.left == null && curr.right == null)) {
+                TreeNode node = stack.pop();
+                list.add(node.val);
+                prev = node;
+                continue;
+            }
+
+            if (curr.right != null) stack.push(curr.right);
+            if (curr.left != null) stack.push(curr.left);
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        PostorderTraversal postorderTraversal = new PostorderTraversal();
+        System.out.println(postorderTraversal.postorderTraversal(stringToTreeNode("[1,null,2,3]")));
+    }
+
+    public static TreeNode stringToTreeNode(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return null;
+        }
+
+        String[] parts = input.split(",");
+        String item = parts[0];
+        TreeNode root = new TreeNode(Integer.parseInt(item));
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+
+        int index = 1;
+        while (!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int leftNumber = Integer.parseInt(item);
+                node.left = new TreeNode(leftNumber);
+                nodeQueue.add(node.left);
+            }
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int rightNumber = Integer.parseInt(item);
+                node.right = new TreeNode(rightNumber);
+                nodeQueue.add(node.right);
+            }
+        }
+        return root;
+    }
+}
